66bac2fdea7aa69cf9515f1707f37868db3a2010
richstacey <richstacey@photon.slc.purestorage.com>
2018-08-04 16:22:01 +0000

    Added simple JSON test validation and examples.

--- example.py
+++ example.py
@@ -2,6 +2,15 @@
 """Example functions and helpers."""
 
 
-def my_cool_function(a: int, b: int):
+def my_cool_function(a: int, b: int) -> int:
     """Add two numbers together."""
     return a + b
+
+
+def count_chars(text: str, char_to_count: str) -> int:
+    """Count the number of times that 'char_to_count' is found in the 'text'."""
+    count = 0
+    for char in text:
+        if char == char_to_count:
+            count += 1
+    return count
